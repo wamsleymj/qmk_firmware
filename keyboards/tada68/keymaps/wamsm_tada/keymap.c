27e150262a4489d09d2118205a28abdf19edda60
Wamsley <wamsm002@wase-am06001345.swna.wdpr.disney.com>
2018-08-20 13:42:07 -0700

    Made space bar act as FUNC when held. Space when tapped. Set CAPSLOCK back to CAPS.

--- keyboards/tada68/keymaps/wamsm_tada/keymap.c
+++ keyboards/tada68/keymaps/wamsm_tada/keymap.c
@@ -21,19 +21,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |FN     |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |Caps   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
-   * |Ctrl|Alt |CMD |        Space          |CMD |Alt|FN |Lef|Dow|Rig |
+   * |Ctrl|Alt |CMD |        FN/Space       |CMD |Alt|FN |Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
 [_BL] = LAYOUT_ansi(
   KC_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  MO(_FL), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
+  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
-  KC_LCTL, KC_LALT,KC_LGUI,                KC_SPC,                        KC_RGUI,KC_RALT,MO(_FL), KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL, KC_LALT,KC_LGUI,                LT(_FL, KC_SPC),                        KC_RGUI,KC_RALT,MO(_FL), KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -43,15 +43,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |      |   |RWD|FFD|PLY|   |HWD|<- |Dn | ->|EWD|   |        |Hme |
    * |----------------------------------------------------------------|
-   * |        |VU-|VU+|MUT|   |   |   |   |BL-|BL+|BL |CAPS  |   |End |
+   * |        |VU-|VU+|MUT|   |   |   |   |BL-|BL+|BL |      |   |End |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |RWD|PLY |FFD|
+   * |    |    |    |                       |   |   |    |RWD|PLY|FFD |
    * `----------------------------------------------------------------'
    */
 [_FL] = LAYOUT_ansi(
   _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, _______,  \
   _______,_______,_______,_______,_______, _______,_______,BWD,KC_UP,FWD,_______,_______,_______, _______,_______ , \
   _______,_______,KC_MRWD,KC_MFFD,KC_MPLY,_______,HWD,KC_LEFT,KC_DOWN,KC_RIGHT,EWD,_______,        _______,KC_HOME, \
-  _______,KC_VOLD, KC_VOLU, KC_MUTE, _______,_______, _______,  _______,BL_DEC, BL_INC, BL_TOGG,KC_CAPS,_______,KC_END, \
+  _______,KC_VOLD, KC_VOLU, KC_MUTE, _______,_______, _______,  _______,BL_DEC, BL_INC, BL_TOGG,_______,_______,KC_END, \
   _______,_______,_______,                 _______,               _______,_______,_______,KC_MRWD, KC_MPLY, KC_MFFD),
 };
