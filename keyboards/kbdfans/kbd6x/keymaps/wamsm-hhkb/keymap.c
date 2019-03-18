38effff15b742d6423e2a6c55399befb5cd66c83
Wamsley <wamsm001@wase-am06001345.swna.wdpr.disney.com>
2019-03-18 13:50:08 -0700

    Added a NAV layer that uses space bar. Home row navigationin addition to traditional HHKB style function layer.

--- keyboards/kbdfans/kbd6x/keymaps/wamsm-hhkb/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/wamsm-hhkb/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-      KC_TRNS, KC_LALT, KC_LGUI,                   KC_SPACE,                 KC_RGUI, TD(TD_RGB_ALT), KC_TRNS
+      KC_TRNS, KC_LALT, KC_LGUI,                   LT(3, KC_SPC),                 KC_RGUI, TD(TD_RGB_ALT), KC_TRNS
       ),
 
     /* Keymap 1: (Function Layer)
@@ -87,6 +87,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS, TO(0), KC_TRNS
+      ),
+
+  /* Keymap 3: (Navigation Layer)
+   * ,-----------------------------------------------------------.
+   * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Del|
+   * |------------------------------------------------------------
+   * |     |   |   |   |   |   |   |   |Up |   |   |   |   |     |
+   * |------------------------------------------------------------
+   * |       |VU-|VU+|MUT|>|||   |   |<- |Dwn| ->|   |   |       |
+   * |------------------------------------------------------------
+   * |        |   |   |   |   |   |   |   |   |   |   |      |   |
+   * '-----------------------------------------------------------'
+   *        |   |    |                        |    |Base|
+   *        `-------------------------------------------'
+   */
+  [3] = LAYOUT(
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
       ),
 };
 
