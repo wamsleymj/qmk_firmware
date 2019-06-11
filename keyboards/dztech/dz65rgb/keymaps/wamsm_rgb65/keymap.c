8d1c9ef84fd337286772639ca5d89dc314370fe2
Michael Wamsley <wamsl@Michaels-MacBook-Pro.local>
2019-06-11 08:36:36 -0700

    Added spacebar function support.

--- keyboards/dztech/dz65rgb/keymaps/wamsm_rgb65/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/wamsm_rgb65/keymap.c
@@ -1,4 +1,4 @@
-#include QMK_KEYBOARD_H
+ b#include QMK_KEYBOARD_H
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    KC_TAB,        KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLASH, KC_DEL,\
 			CTL_T(KC_CAPS),KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,           KC_ENT,    KC_PGUP,\
 		    KC_LSFT,       KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,           KC_UP,     KC_PGDN,\
-		    KC_LCTL, 	   KC_LALT,  KC_LGUI,                KC_SPC,                KC_RGUI, MO(1),    KC_RALT,  KC_LEFT,           KC_DOWN,   KC_RIGHT),
+		    KC_LCTL, 	   KC_LALT,  KC_LGUI,                LT(1, KC_SPC),                KC_RGUI, MO(1),    KC_RALT,  KC_LEFT,           KC_DOWN,   KC_RIGHT),
 
 		/* Keymap _LAYER1: Function Layer
 	   * ,----------------------------------------------------------------.
