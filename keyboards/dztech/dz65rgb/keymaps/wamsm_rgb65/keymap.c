f30e216fa96473b76c254c97389e168ee5d67242
Michael Wamsley <wamsl@Michaels-MacBook-Pro.local>
2019-10-09 18:48:47 -0700

    Updated QMK and personal DZ65 layout.

--- keyboards/dztech/dz65rgb/keymaps/wamsm_rgb65/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/wamsm_rgb65/keymap.c
@@ -10,7 +10,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	    /* Keymap _LAYER0: Default Layer
 		* ,----------------------------------------------------------------.
-		* |~` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp  |Esc |
+		* |~` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp  |~`  |
 		* |----------------------------------------------------------------|
 		* |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
 		* |----------------------------------------------------------------|
@@ -22,7 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		* `----------------------------------------------------------------'
 		*/
 		[_LAYER0] = LAYOUT_65_ansi( /* Base */
+<<<<<<< HEAD
 			KC_GRAVE,       KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,   KC_ESC,\
+=======
+			KC_GESC,       KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,   KC_GRAVE,\
+>>>>>>> 4e63d01a4... Updated QMK and personal DZ65 layout.
 		    KC_TAB,        KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLASH, KC_DEL,\
 			CTL_T(KC_CAPS),KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,           KC_ENT,    KC_PGUP,\
 		    KC_LSFT,       KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,           KC_UP,     KC_PGDN,\
@@ -30,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 		/* Keymap _LAYER1: Function Layer
 	   * ,----------------------------------------------------------------.
-	   * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |    |
+	   * | ~`| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |    |
 	   * |----------------------------------------------------------------|
 	   * |     |   |   |   |   |   |   |   |Up |   |   |   |   |RESET|    |
 	   * |----------------------------------------------------------------|
@@ -44,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_LAYER1] = LAYOUT_65_ansi( /* FN */
 			KC_GRAVE,       KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_TRNS,   KC_TRNS,\
 		    KC_TRNS,       RGB_TOG,  RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, KC_UP,   RGB_VAD, RGB_SPI,  RGB_SPD, KC_TRNS, RESET,    KC_TRNS,\
-			CTL_T(KC_CAPS),KC_TRNS,  KC_MRWD, KC_MFFD, KC_MPLY, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS,  KC_TRNS,          EEP_RST,  KC_TRNS,\
-		    KC_LSFT,       KC_VOLD,  KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_HOME,\
+			CTL_T(KC_CAPS),KC_TRNS,  KC_MRWD, KC_MFFD, KC_MPLY, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS,  KC_TRNS,          EEP_RST,  KC_HOME,\
+		    KC_LSFT,       KC_VOLD,  KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_END,\
 		    KC_TRNS,       KC_TRNS,  KC_TRNS,                  KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_TRNS),
 };
 
